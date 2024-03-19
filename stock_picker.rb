fdd944654dddf5d28e6233ba37fbff356aeba04a
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-03-19 00:25:36 -0300

    Added faster stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -12,5 +12,26 @@ def stock_picker(price_array)
   end
   bestDay
 end
-p stock_picker([17,3,6,9,15,8,6,1,10])
 
+def stock_picker_faster(price_array)
+  bestDay = [0, 0]
+  bestProfit = 0
+  maxArray = []
+  maxPrice = [0, -1]
+  currIdx = price_array.length - 1
+  price_array.reverse_each do |price|
+    maxPrice = [price, currIdx] if price > maxPrice[0]
+    maxArray.unshift(maxPrice)
+    currIdx -= 1
+  end
+  price_array.each_with_index do |buyPrice, buyDay|
+    currProfit = maxArray[buyDay][0] - buyPrice
+    if currProfit > bestProfit
+      bestProfit = currProfit
+      bestDay = [buyDay, maxArray[buyDay][1]]
+    end
+  end
+  bestDay
+end
+
+p stock_picker_faster([17, 3, 6, 9, 15, 8, 6, 1, 10])
